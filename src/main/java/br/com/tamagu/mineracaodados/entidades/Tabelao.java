f853bc0e6ea4b5901daa35122ea3bfba71f47379
marce <marce@192.168.105.69>
2016-10-13 20:34:32 -0300

    projeto mineração de dados

---
+++ src/main/java/br/com/tamagu/mineracaodados/entidades/Tabelao.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.tamagu.mineracaodados.entidades;
+
+/**
+ *
+ * @author Jedi
+ */
+public class Tabelao {
+    
+}
